00c576a1191bfd4f4a85ea48a4f74f1161fcdb35
Aketch Oyugi <aketchoyugi@gmail.com>
2023-06-05 12:30:48 +0000

    chore: spliting settings.py

--- estateManagement/estateManagement/settings/base.py
+++ estateManagement/estateManagement/settings/base.py
@@ -5,7 +5,7 @@ from pathlib import Path
 env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 environ.Env.read_env(BASE_DIR / ".env")
 
@@ -91,12 +91,6 @@ WSGI_APPLICATION = 'estateManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -135,7 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles'
+STATIC_ROOT = BASE_DIR '/static'files
+STATICFILES_DIR = []
+
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- estateManagement/estateManagement/urls.py
+++ estateManagement/estateManagement/urls.py
@@ -1,8 +1,9 @@
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('estate_api.urls')),
+    # path('', include('estate_api.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA)
