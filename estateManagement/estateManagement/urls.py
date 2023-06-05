2cfa3f13baf93a5451ed2cde2b90bd3d26eb2fc4
Aketch Oyugi <aketchoyugi@gmail.com>
2023-06-05 20:41:05 +0000

    feat:  user ID model

--- estateManagement/estateManagement/urls.py
+++ estateManagement/estateManagement/urls.py
@@ -7,3 +7,8 @@ urlpatterns = [
     # path('', include('estate_api.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Estate Management Admin"
+admin.site.site_title = "Estate Management Admin Portal"
+admin.site.index_title = "Welcome to Estate Management Portal"
