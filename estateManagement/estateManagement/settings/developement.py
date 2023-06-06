ad95840422c1b117273ee235135a883379b6123a
Aketch Oyugi <aketchoyugi@gmail.com>
2023-06-06 19:52:45 +0000

    feat: working files

--- estateManagement/estateManagement/settings/developement.py
+++ estateManagement/estateManagement/settings/developement.py
@@ -1,5 +1,15 @@
 from .base import *
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@estatemanagement.com"
+# DOMAIN = env("DOMAIN")
+# SITE_NAME = "Estate Management"
+
 DATABASES = {   
        'default': {
         'ENGINE': 'django.db.backends.sqlite3',   # Database engine
@@ -15,3 +25,4 @@ DATABASES = {
     # }
 
 }
+
--- estateManagement/estateManagement/urls.py
+++ estateManagement/estateManagement/urls.py
@@ -1,14 +1,20 @@
 from django.conf import settings
-from django.contrib import admin
-from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import  include, path
+
 
 urlpatterns = [
-    # path('', include('estate_api.urls')),
+   
     path('admin/', admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 admin.site.site_header = "Estate Management Admin"
 admin.site.site_title = "Estate Management Admin Portal"
 admin.site.index_title = "Welcome to Estate Management Portal"
+
+
+ # path('', include('estate_api.urls')),
