13402c10f55a50165d97d5e078cabbbeaa7a310d
Aketch Oyugi <aketchoyugi@gmail.com>
2023-06-06 04:23:42 +0000

    chore: rectifying the models. py

--- estateManagement/apps/users/models.py
+++ estateManagement/apps/users/models.py
@@ -1,10 +1,11 @@
 import uuid
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixins
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 
-class User(AbstractBaseUser, PermissionsMixins):
+class User(AbstractBaseUser, PermissionsMixin):
     pkid = models.BigAutoField(primary_key=True, editable=False)
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     username = models.CharField(verbose_name=_("Username"), max_length=255, unique=True)
