73df4e4f36fff9a5abce1fe732ecddb3fb72d611
Aketch Oyugi <aketchoyugi@gmail.com>
2023-06-07 11:01:19 +0000

    feat: add ratings

--- estateManagement/apps/enquiries/views.py
+++ estateManagement/apps/enquiries/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from estateManagement.settings.development import DEFAULT_FROM_EMAIL
-
+     
 from .models import Enquiry
 
 
